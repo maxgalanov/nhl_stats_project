ab3f900f4a0b266363ba8d69b56619dd1afe4d37
Nick Boyarckin <70279602+nickboyar@users.noreply.github.com>
2024-01-16 23:54:14 +0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import psycopg2
 
+import datetime
+from dateutil.relativedelta import relativedelta
+
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 from telebot import asyncio_filters
 from telebot.async_telebot import AsyncTeleBot
-
-# list of storages, you can use any storage
 from telebot.asyncio_storage import StateMemoryStorage
 
 # new feature for states.
@@ -17,22 +18,17 @@ from telebot.asyncio_handler_backends import State, StatesGroup
 import asyncio
 
 
-token='6960440682:AAFatXmmDB7JgspcC4b1n-W78H5VkgiOQ3Y'
-#bot=telebot.TeleBot(token)
+token='5793916142:AAEULZu3GI9DiznLRL9DoH0djJy8zDFqWKI'
 
 
 bot = AsyncTeleBot(token, state_storage=StateMemoryStorage())
 
 
 class MyStates(StatesGroup):
-    # Just name variables differently
-    goalie = State() # creating instances of State class is enough from now
-    skater = State()
-    fav_goalie = State()
-    fav_skater = State()
-    goalie_game = State()
-    skater_game = State()
-
+    player = State()
+    name = State()
+    datalens = State()
+    
 
 def gen_df(table):
     conn = psycopg2.connect("""
@@ -51,28 +47,34 @@ def gen_df(table):
 @bot.message_handler(commands=['start'])
 async def start_message(message):
     await bot.send_message(message.chat.id,"""Привет!
-Я могу показать результаты матча за неделю с помощью команды /stats
-Информация по вратарям: /goalies
-Информация по основным игрокам: /skaters""")
+Я могу показать результаты игр за неделю с помощью команды /results
+Информация по игрокам: /player_stats
+Дэшборды в DataLens: /datalens""")
+    
+    imageFile = 'nh.png'
+    
+    img = open(imageFile, 'rb')
+    
+    await bot.send_photo(message.chat.id, img)
     
     
-@bot.message_handler(commands=['stats'])
-async def get_stats(message):
-    stats = pd.read_csv('data/stats.csv')
-    print(stats)
+@bot.message_handler(commands=['results'])
+async def get_results(message):
+    results = gen_df('shedule')
+    
     ans = "Вот матчи, которые прошли на этой неделе: \n"
-    prev_date = stats['date_play'][0]
-    for ind in stats.index:
-        cur_date = stats['date_play'][ind]
+    prev_date = results['date_play'][0]
+    for ind in results.index:
+        cur_date = results['date_play'][ind]
         cur = ''
         if cur_date != prev_date:
             cur += '\n'
-        cur += stats['date_play'][ind] + ' '
-        cur += stats['away_teams'][ind] 
-        cur += ' ' + str(stats['away_result'][ind])
+        cur += results['date_play'][ind] + ' '
+        cur += results['away_teams'][ind] 
+        cur += ' ' + str(results['away_result'][ind])
         cur += ':'
-        cur += str(stats['home_result'][ind]) + ' '
-        cur += stats['home_teams'][ind] 
+        cur += str(results['home_result'][ind]) + ' '
+        cur += results['home_teams'][ind] 
         cur += '.\n'
         ans += cur
         prev_date = cur_date
@@ -80,170 +82,172 @@ async def get_stats(message):
     await bot.send_message(message.chat.id, ans)
     
     
-# @bot.message_handler(commands=['fav_goalie'])
-# async def get_goalies(message):
-
-#     await bot.set_state(message.from_user.id, MyStates.fav_goalie, message.chat.id)    
-#     await bot.send_message(message.chat.id, 'Введите имя Вашего любимого вратаря')
-    
-    
-# @bot.message_handler(state=MyStates.fav_goalie)
-# async def get_goalies(message):
-    
-#     await bot.set_state(message.from_user.id, MyStates.fav_goalie, message.chat.id)    
-#     await bot.send_message(message.chat.id, 'Спасибо, принято!')
-#     async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-#         data['fav_goalie'] = message.text
-    
-
-# @bot.message_handler(commands=['fav_goalie_stats'])
-# async def get_fav_goalie_stats(message):
-    
-#     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        
-#         bot.send_message(message.chat.id, 'Вы ввели ' + data['fav_goalie'] + '.\n')
-
-#         df = gen_df('goalies_agg')
-
-#         res = df[df['playersFIO'] == data['fav_goalie']].iloc[1].to_dict()
-
-#         ans = 'Дата рождения: ' + res['birthDate'] + '\n'
-#         ans += 'Страна: ' + res['birthCountry'] + '\n'
-#         ans += 'Город: ' + res['birthCity'] + '\n'
-#         ans += 'Игр: ' + str(res['gamesCNT']) + '\n'
-#         ans += 'Голов: ' + str(res['goals']) + '\n'
-
-#         bot.send_message(message.chat.id, ans)
 
 
-
-@bot.message_handler(commands=['goalies'])
+@bot.message_handler(commands=['player_stats'])
 async def get_goalies(message):
-
-    await bot.set_state(message.from_user.id, MyStates.goalie, message.chat.id)    
-    await bot.send_message(message.chat.id, 'Введите имя вратаря')
-
     
-@bot.message_handler(state=MyStates.goalie)
-async def get_goalies(message):
-    keys = ["Домашняя", "Выездная"]
+    
+    keys = ["Вратарь", "Полевой игрок"]
     markup = ReplyKeyboardMarkup(resize_keyboard = True)
     row = [KeyboardButton(x) for x in keys]
     markup.add(*row)
-
-    await bot.set_state(message.from_user.id, MyStates.goalie_game, message.chat.id)    
-    await bot.send_message(message.chat.id, 'Домашняя игра или выездная?', reply_markup=markup)
     
-    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['goalie'] = message.text
-
-
-@bot.message_handler(state=MyStates.goalie_game)
-async def name_get(message):
+    await bot.set_state(message.from_user.id, MyStates.player, message.chat.id)    
+    await bot.send_message(message.chat.id, 'Вратарь или полевой игрок?', reply_markup=markup)
     
-    home_game = {}
-    home_game['Домашняя'] = 'H'
-    home_game['Выездная'] = 'R'
+
+        
+@bot.message_handler(state=MyStates.player)
+async def get_g(message):
     
-    print(message.text)
+    
+    print('name?')
+    
+    player_type = {}
+    player_type['Вратарь'] = 'goalies_agg'
+    player_type['Полевой игрок'] = 'skaters_agg'
     
     markup = telebot.types.ReplyKeyboardRemove()
-    
-    df = gen_df('goalies_agg')
-    
-    res = ''
+
+    await bot.set_state(message.from_user.id, MyStates.name, message.chat.id)    
+    await bot.send_message(message.chat.id, 'Введите имя', reply_markup=markup)
     
     async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        res = df[(df['playersFIO'] == data['goalie']) & (df['homeRoadFlag'] == home_game[message.text])].iloc[0].to_dict()
+        print(message.text)
+        data['player'] = player_type[message.text]
+    
+    
+
+@bot.message_handler(state=MyStates.name)
+async def get_stats(message):
+    
+    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+
+    markup = telebot.types.ReplyKeyboardRemove()
+    
+    grp_cols = ['playerId', 'playersFIO', 'triCodeCurrent', 'currentTeamFullName',
+       'teamAbbrev', 'teamFullName', 'birthDate', 'birthCity', 'shootsCatches',
+       'birthCountry', 'birthStateProvince', 'positionCode']
+    
+    res = None
+    
+    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        
+        df = gen_df(data['player'])
+        
+        df = df.drop(columns='homeRoadFlag').groupby(grp_cols, as_index=False).sum()
+        
+        
+        if data['player'] == 'goalies_agg':
+            df['perc_goals'] = round(df['shotsAgainst']/(df['shotsAgainst'] + df['goalsAgainst']) * 100, 2)
+        
+        res = df[df['playersFIO'] == data['name']].iloc[0].to_dict()
+        
+        
+        start_date = datetime.datetime(*[int(i) for i in res['birthDate'].split('-')])
+        end_date =  datetime.date.today()
+        
+        res['difference_in_years'] = str(relativedelta(end_date, start_date).years)
+        
+        
     ans = 'Дата рождения: ' + res['birthDate'] + '\n'
+    ans += 'Возраст: ' + res['difference_in_years'] + '\n'
     ans += 'Страна: ' + res['birthCountry'] + '\n'
     ans += 'Город: ' + res['birthCity'] + '\n'
-    ans += 'Игр: ' + str(res['gamesCNT']) + '\n'
-    ans += 'Голов: ' + str(res['goals']) + '\n'
+    ans += 'Команда: ' + res['teamFullName'] + ' (' + res['teamAbbrev'] + ')' + '\n'
+    
+    ans += 'Игр: ' + str(int(res['gamesCNT'])) + '\n'
+    ans += 'Игр в стартовом составе: ' + str(int(res['gamesStarted'])) + '\n' if data['player'] == 'goalies_agg' else ''
+    
+    ans += 'Игр на ноль: ' + str(int(res['shutouts'])) + '\n' if data['player'] == 'goalies_agg' else ''
+    
+    
+    ans += 'Процент отраженных бросков: ' + str(res['perc_goals']) + '\n' if data['player'] == 'goalies_agg' else ''
+    
+   
+    
+    ans += 'Среднее время на льду: ' + str(int(res['toi'] / res['gamesCNT']) // 60) + ' минут, ' + str(int(res['toi']) % 60) + ' секунд' + '\n'
+    
+    ## TODO
+    ## add for all types
+    ans += 'Очков: ' + str(int(res['points'])) + '\n' if data['player'] == 'skaters_agg' else ''
+    
+    ans += 'Голов: ' + str(int(res['goals'])) + '\n'
+    
+    ans += 'Показатель полезности: ' + str(int(res['plusMinus'])) + '\n' if data['player'] == 'skaters_agg' else ''
+    
+    ans += 'Штрафных минут за сезон: ' + str(int(res['pim'])) + '\n'
+    
     
     await bot.send_message(message.chat.id, ans, reply_markup=markup)
     
-    await bot.send_message(message.chat.id, '''Более подробную информация можете посмотреть в нашем [Дэшбордике по игрокам](https://datalens.yandex/xqnhz02g6x6ml)\!''', parse_mode='MarkdownV2')
+    await bot.send_message(message.chat.id, '''Более подробную информация можете посмотреть в нашем [дашборде по игрокам](https://datalens.yandex/xqnhz02g6x6ml?tab=lD)''', parse_mode='MarkdownV2')
     
     await bot.delete_state(message.from_user.id, message.chat.id)
+        
 
-
-@bot.message_handler(commands=['skaters'])
-async def get_skater(message):
-
-    await bot.set_state(message.from_user.id, MyStates.skater, message.chat.id)    
-    await bot.send_message(message.chat.id, 'Введите имя игрока')
-    
-    
-@bot.message_handler(state=MyStates.skater)
-async def get_goalies(message):
-    keys = ["Домашняя", "Выездная"]
-    markup = ReplyKeyboardMarkup(resize_keyboard = True)
-    row = [KeyboardButton(x) for x in keys]
-    markup.add(*row)
-
-    await bot.set_state(message.from_user.id, MyStates.skater_game, message.chat.id)    
-    await bot.send_message(message.chat.id, 'Домашняя игра или выездная?', reply_markup=markup)
-    
-    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['skater'] = message.text
-
-
-@bot.message_handler(state=MyStates.skater_game)
-async def name_get(message):
-    
-    home_game = {}
-    home_game['Домашняя'] = 'H'
-    home_game['Выездная'] = 'R'
-    
-    print(message.text)
-    
-    markup = telebot.types.ReplyKeyboardRemove()
-    
-    df = gen_df('skaters_agg')
-    res = ''
-    
-    async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        res = df[(df['playersFIO'] == data['skater']) & (df['homeRoadFlag'] == home_game[message.text])].iloc[0].to_dict()
-    ans = 'Дата рождения: ' + res['birthDate'] + '\n'
-    ans += 'Страна: ' + res['birthCountry'] + '\n'
-    ans += 'Город: ' + res['birthCity'] + '\n'
-    ans += 'Игр: ' + str(res['gamesCNT']) + '\n'
-    ans += 'Голов: ' + str(res['goals']) + '\n'
-    
-    await bot.send_message(message.chat.id, ans,  reply_markup=markup)
-    
-    await bot.send_message(message.chat.id, '''Более подробную информация можете посмотреть в нашем [Дэшбордике по игрокам](https://datalens.yandex/xqnhz02g6x6ml)\!''', parse_mode='MarkdownV2')
-    
-    await bot.delete_state(message.from_user.id, message.chat.id)
-    
 
 @bot.message_handler(state="*", commands=['cancel'])
 async def any_state(message):
     await bot.send_message(message.chat.id, "Your state was cancelled.")
     await bot.delete_state(message.from_user.id, message.chat.id)
     
+    
+
+@bot.message_handler(commands=['datalens'])
+async def get_goalies(message):
+    
+    
+    keys = ["Игроки на карте", "Форварды и защитники", "Вратари", "Статистика игрока"]
+    markup = ReplyKeyboardMarkup(resize_keyboard = True)
+    row = [KeyboardButton(x) for x in keys]
+    markup.add(*row)
+    
+    
+    await bot.set_state(message.from_user.id, MyStates.datalens, message.chat.id)    
+    
+    await bot.send_message(message.chat.id, 'Выберите вкладку дэшборда', reply_markup=markup)
+    
+    
+
+@bot.message_handler(state=MyStates.datalens)
+async def get_goalies(message):
+    
+    
+    res = 'https://datalens.yandex/xqnhz02g6x6ml?tab='
+    
+    player_type = {}
+    player_type['Игроки на карте'] = '7pV'
+    player_type['Форварды и защитники'] = 'LD'
+    player_type['Вратари'] = 'Re'
+    player_type['Статистика игрока'] = 'lD'
+    
+    res += player_type[message.text]
+    
+    markup = telebot.types.ReplyKeyboardRemove()
+    
+    
+    await bot.set_state(message.from_user.id, MyStates.datalens, message.chat.id)
+    await bot.send_message(message.chat.id, f'[Дашборд в DataLens]({res})', parse_mode='MarkdownV2')
+    
+    
+    
+    
+    
+    
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
 
 bot.set_my_commands([
-    telebot.types.BotCommand("/start", "main menu"),
-    telebot.types.BotCommand("/stats", "get stats"),
-    telebot.types.BotCommand("/goalies", "get goalies"),
-    telebot.types.BotCommand("/skaters", "get skaters"),
-    telebot.types.BotCommand("/cancel", "cancel"),
-    # telebot.types.BotCommand("/fav_goalie", "pick fav goalie"),
-    # telebot.types.BotCommand("/fav_goalie_stats", "stats fav goalie")
+    telebot.types.BotCommand("/start", "Главное меню"),
+    telebot.types.BotCommand("/results", "Результаты игр за неделю"),
+    telebot.types.BotCommand("/player_stats", "Статистика по игроку"),
+    telebot.types.BotCommand("/datalens", "Дэшборд"),
+    telebot.types.BotCommand("/cancel", "Отмена")
 ])
 
 
 asyncio.run(bot.polling())
 
-# Расписание
-# Любимая команда
-# К ней последние игры 
-# К ней расписание на ближайшую неделю
-# Выбрать любимого игрока
-# Показать стату игрока в даталенс(?)
-# Параметры для игрока 
-# Ссылку на дашборды
-# Свежую статистику по новым матчам
+
